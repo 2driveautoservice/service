b83c46c171bc675541f166d15be5eaf64bf32042
Lamg2799 <43128127+Lamg2799@users.noreply.github.com>
2021-07-03 02:13:25 -0400

    Update deployment script (#9)
    Finished the first version of the deployment script for django and vue, and updated the production database to use secrets again

--- auto_pros_django/auto_pros_django/settings/prod.py
+++ auto_pros_django/auto_pros_django/settings/prod.py
@@ -18,11 +18,11 @@ CORS_ALLOWED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'servicedatabase',
-        'USER': 'luke',
-        'PASSWORD': 'MadebyCAR1!',
-        'HOST': 'localhost',
-        'PORT': ''
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '',
     }
 }
 
