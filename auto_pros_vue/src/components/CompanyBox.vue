8ed0780085fc6655c6ebe26bddad648dc0474345
Luke <lamg2799@gmail.com>
2021-07-13 21:18:25 -0400

    Frontend adjustments

--- auto_pros_vue/src/components/CompanyBox.vue
+++ auto_pros_vue/src/components/CompanyBox.vue
@@ -2,89 +2,71 @@
     <div class="column is-four-fifths py-1">
         <div class="box">
             <article class="media">
-                <div class="media-left mt-3 ml-3">
+                <div class="media-left">
                     <figure class="image">
                         <img v-bind:src="company.get_image">
                     </figure>
                 </div>
-                <div class="media-content ml-5">
-                    <div class="content">
-                        <p style="text-align:left">
-                            <strong style="font-size:1.063rem;">{{ company.name }}</strong> 
-                            <br>
-                            <div class="description-container">
-                                <strong style="font-size:0.875rem">{{ company.get_description }}</strong>
-                            </div>
-                            <br>
-                            User rating:
-                        </p>
-                        <star-rating :rating="company.get_rating" :star-style="starStyle" :is-indicator-active="false"></star-rating>
-                    </div>
+                <div class="media-content">
+                    <p style="text-align:left">
+                        <strong style="font-size:1.063rem;">{{ company.name }}</strong> 
+                        <br>
+                        <strong style="font-size:0.875rem">{{ company.get_description }}</strong>
+                        <br>
+                        <br>
+                        <div style="display:inline-block; align-items: center;">
+                            User Rating:  <star-rating :inline="true" :star-size="20" :read-only="true" :show-rating="false" :rating="company.get_rating"></star-rating>
+                        </div>
+                    </p>
                 </div>
             </article>
         </div>
     </div>
     <div class="column is-one-fifth py-1">
-        <div class="box px-3" id="button-box">
-            <div class="quote" style="margin-bottom:0.938rem">
-                <a class="button is-success" style="font-size:0.938rem;">GET A QUOTE</a>
-            </div>
-            <div class="review">
-                <a class="button" style="font-size:0.938rem;">WRITE A REVIEW</a>
-            </div>
+        <div class="box px-2" id="button-box">
+            <a id="button-quote" class="button is-success">GET A QUOTE</a>
+            <a class="button">WRITE A REVIEW</a>
         </div>
     </div>
 </template>
 
 <style>
-.content {
+.media-content {
     word-break: break-all;
+    margin-left: 10px;
+}
+.media-left {
+    height: auto;
+    width: 20%;
+    margin-top: 0.30rem;
 }
 .button {
     width:100%;
     height: auto;
+    margin-bottom: 0.75rem;
+    white-space: normal;
+}
+#button-quote {
+    margin-top: 0.30rem;
 }
 .box {
     border-radius:0rem;
     height: 100%;
-}
-#button-box {
-    align-items: center;
-}
-.media-left {
-    height: auto;
-    width: 15%;
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
 }
 </style>
 
 <script>
-import StarRating from 'vue-dynamic-star-rating'
+import StarRating from 'vue-star-rating'
 
 export default {
     name: 'CompanyBox',
-    data() {
-        return {
-            starStyle: {
-                fullStarColor: '#ed8a19',
-                emptyStarColor: '#737373',
-                starWidth: 15,
-                starHeight: 25,
-            }
-        }
-    },
     props: {
         company: Object
     },
     components: {
         StarRating
-    },
+    }
 }
 </script>
-
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
---
+++ auto_pros_vue/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="navbar is-white">
+      <div class="navbar-brand">
+          <img src="../assets/media/autopros_logo.png" alt="Logo">
+          <div class="navbar-burger burger" data-target="navMenu" :class="{ 'is-active': isHamburgerOpen }" v-on:click="openHamburgerMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+      </div>
+      <div id="navMenu" class="navbar-menu" :class="{ 'is-active': isHamburgerOpen }">
+        <div class="navbar-start" id="navbar-links">
+          <a class="navbar-item">
+            <p>Browse Categories</p>
+          </a>
+          <a class="navbar-item">
+            <p>Blog</p>
+          </a>
+          <a class="navbar-item">
+            <p>Write a Review</p>
+          </a>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<style>
+.navbar {
+  width: 100%;
+  height: 40px;
+}
+.navbar-brand {
+  width:4%;
+  height:100%;
+  align-items : center;
+}
+.navbar-start {
+  margin-left: 1rem;
+}
+.navbar-burger {
+  margin-left: 10px;
+}
+#navbar-links{
+  margin-left: 0;
+}
+img {
+    min-width: 40px;
+}
+</style>
